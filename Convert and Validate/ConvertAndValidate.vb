83c5e9cca64030f667335cd0a906a32d23e91684
Alexis Villagran <alexisvillagran18@hotmail.com>
2025-02-16 20:35:08 -0700

    Add project files.

---
+++ Convert and Validate/ConvertAndValidate.vb
@@ -0,0 +1,48 @@
+﻿Option Strict On
+Option Compare Binary
+Option Explicit On
+Module ConvertAndValidate
+
+    Sub Main()
+
+        'for testing the function add this to your Sub Main()
+
+        Dim aValidNumber As Integer
+
+        Do
+
+            Console.WriteLine($"Enter a number:")
+
+            usernResponse = Console.ReadLine()
+
+            If ConversionValid(usernResponse, aValidNumber) = True Then
+
+                Console.WriteLine($"{usernResponse} converted successfully to {aValidNumber}!")
+
+            Else
+
+                Console.WriteLine($"Oops, {usernResponse} does not seem to be a number")
+
+            End If
+
+        Loop
+
+    End Sub
+
+    'make this work
+
+    Function ConversionValid(convertThisString, toThisInteger) As Boolean
+
+        Dim status As Boolean
+
+        Try
+
+        Catch ex As Exception
+
+        End Try
+
+        Return status
+    End Function
+
+
+End Module
